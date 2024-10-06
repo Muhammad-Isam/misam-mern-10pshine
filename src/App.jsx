db9cf9956442edfefdbb21b228c74a67f7ace036
Muhammad-Isam <isam.boy@hotmail.com>
2024-10-06 22:55:44 +0500

    Created Login Sign-up Page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AuthorisationForm from './components/AuthorisationForm'
+import './components/AuthorisationForm.css'
+function App() {
+  return (
+    <div>
+      <AuthorisationForm/>
+    </div>
+  )
+}
+
+export default App
